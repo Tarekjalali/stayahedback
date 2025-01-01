9f8256a163a080da0d9b0d963ffc373df927b4f2
Tarekjalali <jlali.tare9@gmail.com>
2025-01-01 16:11:03 +0100

    test2

--- Server.js
+++ Server.js
@@ -19,7 +19,7 @@ app.use(cors({
     credentials: true 
 }));
 
-cron.schedule('10 15 * * *', async () => { 
+cron.schedule('15 15 * * *', async () => { 
     console.log(`Cron job started at: ${new Date()}`); 
 
     const today = new Date().toISOString().split('T')[0]; 
